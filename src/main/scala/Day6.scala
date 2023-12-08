6747b5bd78913ed8fe0a71d8cd455faf4311bb3f
Velizar Hristov <velizar.hs@gmail.com>
2023-12-08 14:52:14 +0200

    Day 6

---
+++ src/main/scala/Day6.scala
@@ -0,0 +1,17 @@
+import scala.io.Source
+
+@main
+def day6(): Unit = {
+  val fish = Source.fromFile("resources/6").getLines.next.split(",").map(_.toInt).toBuffer
+  for (_ <- 1 to 80) {
+    for (i <- fish.indices) {
+      if (fish(i) == 0) {
+        fish(i) = 6
+        fish.append(8)
+      } else {
+        fish(i) -= 1
+      }
+    }
+  }
+  println(fish.size)
+}
---
+++ src/main/scala/Day6_2.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+
+@main
+def day6_2(): Unit = {
+  var fishOfDays = Array.fill(9)(0L)
+  for (fish <- Source.fromFile("resources/6").getLines.next.split(",").map(_.toInt))
+    fishOfDays(fish) += 1
+  for (_ <- 1 to 256) {
+    val atZero = fishOfDays.head
+    fishOfDays = fishOfDays.tail :+ 0
+    fishOfDays(6) += atZero
+    fishOfDays(8) += atZero
+  }
+  println(fishOfDays.sum)
+}
