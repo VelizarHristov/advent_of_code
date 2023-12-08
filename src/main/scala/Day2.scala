0651b437b36caccf64897f623374086ce1b6295a
Velizar Hristov <velizar.hs@gmail.com>
2023-12-08 10:55:09 +0200

    Day 2

---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,17 @@
+import scala.io.Source
+
+@main
+def day2(): Unit = {
+  var horizontalPosition = 0
+  var depth = 0
+  for (line <- Source.fromFile("resources/2").getLines) {
+    val Array(direction, amountStr) = line.split(" ")
+    val amount = amountStr.toInt
+    direction match {
+      case "forward" => horizontalPosition += amount
+      case "down" => depth += amount
+      case "up" => depth -= amount
+    }
+  }
+  println(horizontalPosition * depth)
+}
---
+++ src/main/scala/Day2_2.scala
@@ -0,0 +1,20 @@
+import scala.io.Source
+
+@main
+def day2_2(): Unit = {
+  var horizontalPosition = 0
+  var depth = 0
+  var aim = 0
+  for (line <- Source.fromFile("resources/2").getLines) {
+    val Array(direction, amountStr) = line.split(" ")
+    val amount = amountStr.toInt
+    direction match {
+      case "forward" =>
+        horizontalPosition += amount
+        depth += aim * amount
+      case "down" => aim += amount
+      case "up" => aim -= amount
+    }
+  }
+  println(horizontalPosition * depth)
+}
