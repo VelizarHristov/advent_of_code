021c46850fab1b776feabc9d2289cb9565e594a0
Velizar Hristov <velizar.hs@gmail.com>
2023-12-09 10:06:34 +0200

    Day 11

---
+++ src/main/scala/Day11.scala
@@ -0,0 +1,36 @@
+import scala.io.Source
+
+@main
+def day11(): Unit = {
+  val octopi = Source.fromFile("resources/11").getLines.toArray.map(_.split("").map(_.toInt))
+  var flashes = 0
+  for (_ <- 1 to 100) {
+    var willFlash = Set.empty[(Int, Int)]
+    for (i <- octopi.indices; j <- octopi(i).indices) {
+      octopi(i)(j) += 1
+      if (octopi(i)(j) == 10)
+        willFlash += ((i, j))
+    }
+    while (willFlash.nonEmpty) {
+      val next = willFlash
+      willFlash = Set.empty
+      for {
+        (x, y) <- next
+        dx <- Seq(-1, 0, 1)
+        dy <- Seq(-1, 0, 1)
+        if !(dx == 0 && dy == 0)
+        if octopi.lift(x + dx).getOrElse(Array.empty[Int]).lift(y + dy).nonEmpty
+      } {
+        octopi(x + dx)(y + dy) += 1
+        if (octopi(x + dx)(y + dy) == 10) {
+          willFlash += ((x + dx, y + dy))
+        }
+      }
+    }
+    for (i <- octopi.indices; j <- octopi(i).indices; if octopi(i)(j) >= 10) {
+      octopi(i)(j) = 0
+      flashes += 1
+    }
+  }
+  println(flashes)
+}
---
+++ src/main/scala/Day11_2.scala
@@ -0,0 +1,35 @@
+import scala.io.Source
+
+@main
+def day11_2(): Unit = {
+  val octopi = Source.fromFile("resources/11").getLines.toArray.map(_.split("").map(_.toInt))
+  var steps = 0
+  while (octopi.flatten.exists(_ != 0)) {
+    var willFlash = Set.empty[(Int, Int)]
+    for (i <- octopi.indices; j <- octopi(i).indices) {
+      octopi(i)(j) += 1
+      if (octopi(i)(j) == 10)
+        willFlash += ((i, j))
+    }
+    while (willFlash.nonEmpty) {
+      val next = willFlash
+      willFlash = Set.empty
+      for {
+        (x, y) <- next
+        dx <- Seq(-1, 0, 1)
+        dy <- Seq(-1, 0, 1)
+        if !(dx == 0 && dy == 0)
+        if octopi.lift(x + dx).getOrElse(Array.empty[Int]).lift(y + dy).nonEmpty
+      } {
+        octopi(x + dx)(y + dy) += 1
+        if (octopi(x + dx)(y + dy) == 10) {
+          willFlash += ((x + dx, y + dy))
+        }
+      }
+    }
+    for (i <- octopi.indices; j <- octopi(i).indices; if octopi(i)(j) >= 10)
+      octopi(i)(j) = 0
+    steps += 1
+  }
+  println(steps)
+}
