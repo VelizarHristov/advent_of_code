58fe2bd3e5513834c204238988a4e79fb1056d1b
Velizar Hristov <velizar.hs@gmail.com>
2023-12-11 12:21:44 +0200

    Day 11 for AoC 2023

---
+++ src/main/scala/Day11_2023.scala
@@ -0,0 +1,35 @@
+import scala.collection.mutable
+import scala.io.Source
+
+@main
+def day11_2023(): Unit = {
+  var grid = Source.fromFile("resources/11_2023").getLines.map(line => {
+    line.map(_ == '#').to(mutable.ArraySeq)
+  }).to(mutable.ArraySeq)
+  var added = 0
+  for (i <- grid.indices) {
+    if (grid(i + added).forall(i => !i)) {
+      grid = (grid.take(i + added) :+ mutable.ArraySeq.fill(grid.head.size)(false)) ++ grid.drop(i + added)
+      added += 1
+    }
+  }
+
+  val colsToExpand = grid.transpose.zipWithIndex.filter { case (a, b) => a.forall(x => !x)}.map(_._2)
+  for (i <- grid.indices) {
+    var added = 0
+    for (colToAdd <- colsToExpand) {
+      grid(i) = (grid(i).take(added + colToAdd) :+ false) ++ grid(i).drop(added + colToAdd)
+      added += 1
+    }
+  }
+  val galaxyCoords = for {
+    i <- grid.indices
+    j <- grid(i).indices
+    if grid(i)(j)
+  } yield (i, j)
+  val distances = for {
+    (y1, x1) <- galaxyCoords
+    (y2, x2) <- galaxyCoords
+  } yield (y1 - y2).abs + (x1 - x2).abs
+  println(distances.sum / 2)
+}
---
+++ src/main/scala/Day11_2_2023.scala
@@ -0,0 +1,35 @@
+import scala.collection.mutable
+import scala.io.Source
+
+@main
+def day11_2_2023(): Unit = {
+  val grid = Source.fromFile("resources/11_2023").getLines.map(line => {
+    line.map(_ == '#').to(mutable.ArraySeq)
+  }).to(mutable.ArraySeq)
+  var added = 0
+  val bigRows = grid.transpose.indices.filter(i => grid(i).forall(x => !x)).toSet
+  val bigCols = grid.indices.filter(i => grid.transpose.apply(i).forall(x => !x)).toSet
+  val galaxyCoords = for {
+    i <- grid.indices
+    j <- grid(i).indices
+    if grid(i)(j)
+  } yield (i, j)
+  val (bigDistances, smallDistances) = (for {
+    a@(y1, x1) <- galaxyCoords
+    b@(y2, x2) <- galaxyCoords
+    if a != b
+  } yield {
+    val maxY = y1 max y2
+    val minY = y1 min y2
+    val maxX = x1 max x2
+    val minX = x1 min x2
+    val bigRowCount = (minY to maxY).count(bigRows.contains)
+    val smallRowCount = maxY - minY - bigRowCount
+    val bigColCount = (minX to maxX).count(bigCols.contains)
+    val smallColCount = maxX - minX - bigColCount
+    (bigRowCount + bigColCount, smallRowCount + smallColCount)
+  }).unzip
+  val bigSum = bigDistances.sum / 2
+  val smallSum = smallDistances.sum / 2
+  println(BigInt(bigSum) * 1000000 + smallSum)
+}
