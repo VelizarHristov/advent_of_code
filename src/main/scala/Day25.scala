a557830bc50382ccc36f3f02713c83f7e483121d
Velizar Hristov <velizar.hs@gmail.com>
2024-01-14 14:41:28 +0200

    Day 25

---
+++ src/main/scala/Day25.scala
@@ -0,0 +1,32 @@
+import io.Source
+
+@main
+def day25(): Unit = {
+  var grid = Source.fromFile("resources/25").getLines.map(_.toCharArray).toArray
+  var ticks = 0
+  var hasMoved = true
+  val nextGrid = grid.map(_.clone())
+  while (hasMoved) {
+    hasMoved = false
+    grid = nextGrid.map(_.clone())
+    for (i <- grid.indices; j <- grid(i).indices; if grid(i)(j) == '>') {
+      val next = (j + 1) % grid(i).length
+      if (grid(i)(next) == '.') {
+        nextGrid(i)(next) = '>'
+        nextGrid(i)(j) = '.'
+        hasMoved = true
+      }
+    }
+    grid = nextGrid.map(_.clone())
+    for (i <- grid.indices; j <- grid(i).indices; if grid(i)(j) == 'v') {
+      val next = (i + 1) % grid.length
+      if (grid(next)(j) == '.') {
+        nextGrid(next)(j) = 'v'
+        nextGrid(i)(j) = '.'
+        hasMoved = true
+      }
+    }
+    ticks += 1
+  }
+  println(ticks)
+}
