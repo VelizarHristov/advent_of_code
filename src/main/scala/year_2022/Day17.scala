c7f348a8063f200a856f8adb88dea78f81409ef3
Velizar Hristov <velizar.hs@gmail.com>
2024-07-21 08:37:06 +0300

    Year 2022, day 17 - refactor

--- src/main/scala/year_2022/Day17.scala
+++ src/main/scala/year_2022/Day17.scala
@@ -6,18 +6,44 @@ import io.Source
 def day17(): Unit = {
   val dirs = Source.fromFile("resources/2022/17").getLines().next
     .map(c => if (c == '>') 1 else -1).toArray
-  val present = collection.mutable.Set((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0))
+  val present = (0 to 7).map((_, 0)).to(collection.mutable.Set)
+  val shapes = Array(
+"""
+####
+""",
+
+"""
+.#.
+#.#
+.#.
+""", // the middle was removed because it makes no difference
+
+"""
+..#
+..#
+###
+""",
+
+"""
+#
+#
+#
+#
+""",
+
+"""
+##
+##
+""").map(str => {
+    val lines = str.split("\n").map(_.toCharArray)
+    for (y <- lines.indices; x <- lines(y).indices; if lines(y)(x) == '#')
+      yield (x + 2, lines.length - y - 1)
+  })
   var dirIdx = 0
 
   for (rockNum <- 0 until 2022) {
     val startY = present.map(_._2).max + 4
-    var positions = rockNum % 5 match {
-      case 0 => Seq((2, startY), (3, startY), (4, startY), (5, startY))
-      case 1 => Seq((3, startY), (2, startY + 1), (4, startY + 1), (3, startY + 2))
-      case 2 => Seq((2, startY), (3, startY), (4, startY), (4, startY + 1), (4, startY + 2))
-      case 3 => Seq((2, startY), (2, startY + 1), (2, startY + 2), (2, startY + 3))
-      case 4 => Seq((2, startY), (3, startY), (2, startY + 1), (3, startY + 1))
-    }
+    var positions = shapes(rockNum % 5).map { case (x, y) => (x, y + startY) }
     while {
       val nextPos = positions.map { case (x, y) => (x + dirs(dirIdx), y) }
       if (nextPos.forall { case (x, y) => x >= 0 && x <= 6 && !present.contains((x, y)) })
