92eb8f1b680bd431045e7b2f15b4dcdccbbdf0d1
Velizar Hristov <velizar.hs@gmail.com>
2024-12-08 07:30:46 +0200

    Year 2024, day 8

---
+++ src/main/scala/year_2024/Day8.scala
@@ -0,0 +1,26 @@
+package year_2024
+
+import io.Source
+
+import year_2022.Helpers._
+
+@main
+def day8(): Unit = {
+  val grid = Source.fromFile("resources/2024/8").getLines.toArray.map(_.toCharArray)
+  val symbols = grid.flatten.distinct.filterNot("#.".contains)
+  val symCoords = symbols.map(sym =>
+    for (i <- grid.indices; j <- grid(i).indices; if grid(i)(j) == sym)
+      yield (i, j))
+  val antinodes = for {
+    coords <- symCoords
+    coord1 <- coords
+    coord2 <- coords
+    if coord1 != coord2
+    (dy, dx) = (coord1 - coord2)
+    (y0, x0) = coord1
+    y = y0 + dy
+    x = x0 + dx
+    if safeGet(grid, y, x).isDefined
+  } yield (y, x)
+  println(antinodes.distinct.size)
+}
---
+++ src/main/scala/year_2024/Day8_2.scala
@@ -0,0 +1,27 @@
+package year_2024
+
+import io.Source
+
+import year_2022.Helpers._
+
+@main
+def day8_2(): Unit = {
+  val grid = Source.fromFile("resources/2024/8").getLines.toArray.map(_.toCharArray)
+  val symbols = grid.flatten.distinct.filterNot("#.".contains)
+  val symCoords = symbols.map(sym =>
+    for (i <- grid.indices; j <- grid(i).indices; if grid(i)(j) == sym)
+      yield (i, j))
+  val antinodes = for {
+    coords <- symCoords
+    coord1 <- coords
+    coord2 <- coords
+    if coord1 != coord2
+    (dy, dx) = (coord1 - coord2)
+    (y0, x0) = coord1
+    mult <- (0 until (grid.size max grid(0).size))
+    y = y0 + mult * dy
+    x = x0 + mult * dx
+    if safeGet(grid, y, x).isDefined
+  } yield (y, x)
+  println(antinodes.distinct.size)
+}
