d0a7de5274085592d1c4c5bf34ae827744b22de6
Velizar Hristov <velizar.hs@gmail.com>
2024-12-22 14:09:23 +0200

    Year 2024, day 14 - refactor

--- src/main/scala/year_2024/Day14_2.scala
+++ src/main/scala/year_2024/Day14_2.scala
@@ -11,25 +11,25 @@ def day14_2(): Unit = {
     val Array(pos, vel) = line.split(' ').map(_.drop(2).split(',').map(_.toInt))
     (pos, vel)
   }).toVector
-  var i = 1
+  var res = 1
   while (true) {
     robots = robots.map((pos, vel) => {
       val nextPos = bounds.indices.map(i => wrapMod(pos(i) + vel(i), bounds(i)))
       (nextPos.toArray, vel)
     })
 
-    val grid = Array.fill(bounds(1))(Array.fill(bounds(0))('.'))
-    for ((Array(x, y), _) <- robots)
-      grid(y)(x) = '#'
     // no idea why this worked
     // it wasn't in the problem description and it doesn't even make sense
-    if (grid.flatten.count(_ == '#') == robots.size) {
-      for (line <- grid) {
+    val positions = robots.map(_._1.toVector).distinct
+    if (positions.size == robots.size) {
+      val grid = Array.fill(bounds(1))(Array.fill(bounds(0))('.'))
+      for (Vector(x, y) <- positions)
+        grid(y)(x) = '#'
+      for (line <- grid)
         println(line.mkString)
-      }
-      println(i)
+      println(res)
       System.exit(0)
     }
-    i += 1
+    res += 1
   }
 }
