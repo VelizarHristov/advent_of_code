dbe7cce2cc9ccd7c191b250551dad5855f227c24
Velizar Hristov <velizar.hs@gmail.com>
2025-10-07 13:33:10 +0300

    Fix compilation error

--- src/main/scala/year_2024/Day7_2.scala
+++ src/main/scala/year_2024/Day7_2.scala
@@ -2,17 +2,16 @@ package year_2024
 
 import io.Source
 
-def canEqualTotal(nums: List[Long], total: Long, acc: Long = 0): Boolean = nums match {
-  case Nil              => total == acc
-  case _ if acc > total => false
-  case n :: rest        =>
-    canEqualTotal(rest, total, acc + n) ||
-    canEqualTotal(rest, total, acc * n) ||
-    canEqualTotal(rest, total, (acc.toString + n.toString).toLong)
-}
-
 @main
 def day7_2(): Unit = {
+  def canEqualTotal(nums: List[Long], total: Long, acc: Long = 0): Boolean = nums match
+    case Nil              => total == acc
+    case _ if acc > total => false
+    case n :: rest        =>
+      canEqualTotal(rest, total, acc + n) ||
+      canEqualTotal(rest, total, acc * n) ||
+      canEqualTotal(rest, total, (acc.toString + n.toString).toLong)
+
   val res = Source.fromFile("resources/2024/7").getLines.flatMap(line => {
     val Array(firstStr, rest) = line.split(": ")
     val total = firstStr.toLong
